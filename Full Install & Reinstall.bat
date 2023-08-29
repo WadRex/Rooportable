3ec8b6f7dee37009c58a91586cb8308bc7bbdc7e
WadRex <wadrex@outlook.com>
2023-08-29 20:04:53 +0200

    Fix 'Error 401'

--- Full Install & Reinstall.bat
+++ Full Install & Reinstall.bat
@@ -100,6 +100,8 @@ mkdir ".\Roop\models\buffalo_l"
 tar -xf ".\Temporary Files\buffalo_l.zip" -C ".\Roop\models\buffalo_l"
 rd /s /q ".\Temporary Files"
 
+bitsadmin /transfer "Download 'inswapper' Model" /download /priority normal "https://github.com/WadRex/Rooportable/releases/download/inswapper_128.onnx/inswapper_128.onnx" "%~dp0\Roop\models\inswapper_128.onnx"
+
 cls
 title "Full Install & Reinstall - Completed"
 mode con:cols=71 lines=9
